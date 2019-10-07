2f1ce99f1e01dc7ec51da5f8df2b8d87ce446e52
meteyarici <meteyarici@gmail.com>
2019-10-07 14:20:09 +0300

    adding a test

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -8,13 +8,8 @@ class HomeCest
 {
     public function checkHome(AcceptanceTester $I)
     {
-        $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('My Application');
+        //$I->amOnPage(Url::toRoute('/v1/user/login'));
+        //$I->see('Unauthorized');
 
-        $I->seeLink('About');
-        $I->click('About');
-        $I->wait(2); // wait for page to be opened
-
-        $I->see('This is the About page.');
     }
 }
